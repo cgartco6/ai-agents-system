2c4b2822d894cac56b6f1750545b2efc2ceeba71
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-11-19 08:53:44 +0200

    Create init.py

---
+++ brain/agents/coding_agents/init.py
@@ -0,0 +1,11 @@
+from .code_generation_agent import CodeGenerationAgent
+from .code_review_agent import CodeReviewAgent
+from .architecture_agent import ArchitectureAgent
+from .debugging_agent import DebuggingAgent
+
+__all__ = [
+    'CodeGenerationAgent',
+    'CodeReviewAgent', 
+    'ArchitectureAgent',
+    'DebuggingAgent'
+]
